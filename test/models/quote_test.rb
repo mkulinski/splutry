0c9b4f585ea9c1dc40bf6934435cd036dd1a7143
Michael Kulinski <me@michaelkulinski.com>
2016-01-05 00:55:45 +0000

    wrote a test and then created a new unique id field for the quotes

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "unique_tag" do 
+    quote = Quote.create(:author => 'Hunter S. Thompson', :saying => 'A word to the wise is infuriating.')
+    expected = 'HST#' + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
   end
 end
